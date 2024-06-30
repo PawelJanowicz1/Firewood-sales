e98fd7d4ce852139af0a651dcfabea8f0f360c35
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-06-30 19:24:13 +0200

    Initial commit

---
+++ src/main/java/org/example/firewoodsaless/FirewoodSalessApplication.java
@@ -0,0 +1,13 @@
+package org.example.firewoodsaless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirewoodSalessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirewoodSalessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/firewoodsaless/FirewoodSalessApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.firewoodsaless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirewoodSalessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
