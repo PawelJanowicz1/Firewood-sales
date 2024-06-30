895f5d535eb434e10b35648c5546cef3c78509ed
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-06-30 19:44:36 +0200

    changes

--- src/main/java/org/example/firewoodsales/FirewoodSalesApplication.java
+++ src/main/java/org/example/firewoodsales/FirewoodSalesApplication.java
@@ -1,13 +1,13 @@
-package org.example.firewoodsaless;
+package org.example.firewoodsales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirewoodSalessApplication {
+public class FirewoodSalesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FirewoodSalessApplication.class, args);
+        SpringApplication.run(FirewoodSalesApplication.class, args);
     }
 
 }
--- src/test/java/org/example/firewoodsales/FirewoodSalesApplicationTests.java
+++ src/test/java/org/example/firewoodsales/FirewoodSalesApplicationTests.java
@@ -1,10 +1,10 @@
-package org.example.firewoodsaless;
+package org.example.firewoodsales;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FirewoodSalessApplicationTests {
+class FirewoodSalesApplicationTests {
 
     @Test
     void contextLoads() {
