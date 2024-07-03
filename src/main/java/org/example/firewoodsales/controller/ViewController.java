55dcc5b08bb82b6d5732366cdea582e0e6dff32e
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-07-03 17:12:54 +0200

    fixing contact form

--- src/main/java/org/example/firewoodsales/controller/ViewController.java
+++ src/main/java/org/example/firewoodsales/controller/ViewController.java
@@ -6,6 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller("/")
 public class ViewController {
 
+    @GetMapping("/offer")
+    String offerView(){
+        return "offer";
+    }
+
+    @GetMapping("/pricing")
+    String pricingView(){
+        return "pricing";
+    }
+
     @GetMapping("/contact")
     String contactView(){
         return "contact";
